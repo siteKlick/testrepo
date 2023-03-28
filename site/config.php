cdea8d087daa1e39adf72ee272a3d5ee19073d64
marcogeue <129186157+marcogeue@users.noreply.github.com>
2023-03-28 15:53:27 +0200

    Initial commit

---
+++ site/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'debug'  => true,
+    'panel' => [
+        'install' => true,
+        'css' => 'assets/css/custom-panel.css'
+    ],
+    'smartypants' => [
+        'doublequote.open' => '&#8222;',
+        'doublequote.close' => '&#8220;'
+    ]
+];
+
+?>
